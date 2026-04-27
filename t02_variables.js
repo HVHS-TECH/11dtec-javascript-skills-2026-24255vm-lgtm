84215a45e984cba8e4e8c330f28f41df2729cfe0
24255vm <24255vm@hvhs.school.nz>
2026-04-28 10:56:34 +1200

    doing maths

--- t02_variables.js
+++ t02_variables.js
@@ -8,10 +8,10 @@ console.log("Find me")
 
 var year="2026"
 var name="Vaibhav"
-var pocketmoney=20;
-var age=15
+var pocketmoney="20"
+var age="15"
 var isPlayer= true;
-console.log("Hi "+ name +" as of" + year +" you are " + age +);
+console.log("Hi "+ name +" as of " + year +" you are " + age + " you have " + pocketmoney );
 
 
 
--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,47 @@
-/* Paste your code from task 2 here */
+/****************************
+This is my intro
+****************************/
+console.log("Variables")
+console.log("Find me")
+
+//Variables
+
+var year="2026"
+var name="Vaibhav"
+var pocketmoney=20
+var age=15
+var born=2010
+var ten=10
+var two=2
+var isPlayer= true;
+var futureage=age+ten
+var halfmoney=pocketmoney/two
+var halfmoneyplusthree=
+console.log("Hi "+ name +" as of " + year +" you are " + age + " years old " + " I was born in " + born +" in 10 years you will be " + futureage +
+            " you have " + pocketmoney + " dollars. you spend half of your money, now you have " + halfmoney + "then you get 3$, now you have" + halfmoneyplusthree );
+
+var num1=10;
+var num2=30;
+var answer;
+
+answer=num1+num2
+console.log("add", answer);
+answer=num1-num2
+console.log("subtract", answer);
+answer=num1*num2
+console.log("multiply", answer);
+answer=num2/num1
+console.log("divide", answer);
+
+/****************************
+Main code
+****************************/
+
+
+
+
+
+
+/****************************
+Functions
+****************************/
